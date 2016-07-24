4fbc5a5815aea231520b43b4239415fabd678769
Faye Cheng <fayehcheng@gmail.com>
2016-07-23 17:49:05 -0700

    Added routes to offering and roommate to hello.py, updated index page

--- hello.py
+++ hello.py
@@ -13,6 +13,12 @@ def userProfile (username):
 @app.route("/login"):
 def loginPage ():
 
+@app.route("/offering"):
+def offering ():
+
+@app.route("/roommate"):
+def roommate ():
+
 
 if __name__ == "__main__":
     app.run()
